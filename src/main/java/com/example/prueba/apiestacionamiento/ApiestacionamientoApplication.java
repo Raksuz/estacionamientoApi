c587c7187749091ebc82cb471212c14ed82c08eb
Carlos Lopez <cxrloz.1112@gmail.com>
2024-03-05 08:12:27 -0600

    Iniciando proyecto de la prueba

---
+++ src/main/java/com/example/prueba/apiestacionamiento/ApiestacionamientoApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba.apiestacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiestacionamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiestacionamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prueba/apiestacionamiento/ApiestacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba.apiestacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiestacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
